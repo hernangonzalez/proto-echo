33af8a67f6daae60a311a082a60e115186360f42
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-02-24 20:30:57 +0000

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use futures::SinkExt;
+use tokio::{
+    net::{TcpListener, TcpStream},
+    task::JoinSet,
+};
+use tokio_stream::StreamExt;
+use tokio_util::codec::{BytesCodec, Framed};
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    let mut handles = JoinSet::new();
+
+    while let core::result::Result::Ok(res) = listener.accept().await {
+        let stream = res.0;
+        handles.spawn(async move { work_connection(stream).await });
+    }
+
+    while (handles.join_next().await).is_some() {}
+
+    Ok(())
+}
+
+async fn work_connection(stream: TcpStream) -> Result<()> {
+    let mut framed = Framed::new(stream, BytesCodec::new());
+    while let Some(message) = framed.next().await {
+        let bytes = message?;
+        framed.send(bytes).await?;
+    }
+
+    Ok(())
 }
